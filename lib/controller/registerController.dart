cd06801a7117615c967c68c442717b3a50f5be7f
dissingar djino <dissingardjino@icloud.com>
2020-06-16 21:37:30 +0200

    Authentification par sms finalisé

--- lib/controller/registerController.dart
+++ lib/controller/registerController.dart
@@ -1,6 +1,8 @@
 
 
 import 'package:africars/controller/administrationController.dart';
+import 'package:africars/fonction/firebaseHelper.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:intl_phone_number_input/intl_phone_number_input.dart';
@@ -16,10 +18,13 @@ class registerController extends StatefulWidget{
 
 class homeRegister extends State<registerController>{
   final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  final auth=FirebaseAuth.instance;
 
   final TextEditingController controller = TextEditingController();
   String initialCountry = 'NG';
   PhoneNumber number= PhoneNumber(isoCode: 'ML');
+  String smsCode,verificationId;
+  bool codeSent=false;
 
   @override
   Widget build(BuildContext context) {
@@ -73,10 +78,30 @@ class homeRegister extends State<registerController>{
             ),
           ),
           Padding(padding: EdgeInsets.all(10)),
+          (codeSent)?TextField(
+            decoration: InputDecoration(
+
+              hintText: 'Entrer le code'
+
+            ),
+            onChanged: (value){
+              setState(() {
+                smsCode=value;
+              });
+            },
+
+          ):Container(),
         
           FlatButton(
-            onPressed: ()=>validationPhone(number.toString()),
-            child: Text('Validation'),
+            onPressed: (){
+              (codeSent)?handleOTP(smsCode, verificationId):handleSignWithSign(number.toString());
+
+
+                //firebaseHelper().handleSignPhone(number.toString());
+            },
+            child: Text('Validation')
+              
+
           ),
 
         ],
@@ -86,19 +111,69 @@ class homeRegister extends State<registerController>{
   }
 
 
-  validationPhone(String number){
-    return showDialog(
-      context: context,
-      barrierDismissible: false,
-      builder: (BuildContext context)
-        {
-          return AlertDialog(
-            title: Text(number),
-          );
-        }
 
 
-    );
-  }
+
+
+
+  //authentification
+Future <void> handleSignWithSign(phone)async
+{
+  final PhoneVerificationCompleted verified = (AuthCredential credential){
+    auth.signInWithCredential(credential);
+
+  };
+
+  final PhoneVerificationFailed failed =(AuthException exception){
+    print(exception);
+  };
+
+
+  final PhoneCodeSent phoneCodeSent =(String verifId,[int forceResend]){
+    this.verificationId = verifId;
+    setState(() {
+      codeSent=true;
+    });
+
+
+  };
+
+  final PhoneCodeAutoRetrievalTimeout autoRetrievalTimeout =(String verifId){
+    this.verificationId = verifId;
+
+  };
+
+
+  auth.verifyPhoneNumber(
+      phoneNumber: phone,
+      timeout: Duration(seconds: 60),
+      verificationCompleted: verified,
+      verificationFailed: failed,
+      codeSent: phoneCodeSent,
+      codeAutoRetrievalTimeout: autoRetrievalTimeout
+  );
+
+
+}
+
+
+Future <void> handleOTP(smsCode,verifId) async{
+  AuthCredential authCredential=PhoneAuthProvider.getCredential(
+      verificationId: verifId,
+      smsCode: smsCode);
+  AuthResult authResult= await FirebaseAuth.instance.signInWithCredential(authCredential);
+  FirebaseUser user =authResult.user;
+  String uid = user.uid;
+  Map map ={
+    'uid':uid,
+    'login':'',
+    'prenom':'',
+    'nom':'',
+    'mail':'',
+    'typeUtilisateur':''
+
+  };
+  firebaseHelper().addUser(uid, map);
+}
 
 }
--- lib/fonction/firebaseHelper.dart
+++ lib/fonction/firebaseHelper.dart
@@ -1,5 +1,7 @@
 
+import 'package:africars/model/utilisateur.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
 
 class firebaseHelper{
 //authetification
@@ -39,7 +41,49 @@ Future <void> handleSignPhone(String phone) async
       codeSent: phoneCodeSent,
       codeAutoRetrievalTimeout: autoRetrievalTimeout
   );
+
 }
+
+
+Future<void> signOTP(smsCode,verifId)async{
+  AuthCredential authCredential=PhoneAuthProvider.getCredential(
+      verificationId: verifId,
+      smsCode: smsCode);
+  AuthResult authResult = await auth.signInWithCredential(authCredential);
+  FirebaseUser user=authResult.user;
+  String uid=user.uid;
+  Map map={
+    'nom':'',
+    'prenom':'',
+    'id':'',
+    'compagnie':'',
+    'telephone':'',
+    'image':'',
+    'type_utilisateur':'',
+  };
+
+}
+
+
+//database
+  static final base=FirebaseDatabase.instance.reference();
+  final base_user=base.child("utilisateur");
+
+
+
+
+
+  addUser(String uid,Map map)
+  {
+    base_user.child(uid).set(map);
+  }
+
+
+  Future<utilisateur> getUser(String uid) async
+  {
+    DataSnapshot snapshot = await base_user.child(uid).once();
+    return utilisateur(snapshot);
+  }
 
 
 
